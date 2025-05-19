b68d660378263d072b34ca146b5a64353be65eb9
Siddharth Raj <rajsiddharth901@gmail.com>
2025-05-19 19:47:26 +0530

    methods.cpp added

---
+++ self_progress/methods.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class car{
+    public:
+    void speedOfCar(int speedInkm);
+};
+
+
+void car::speedOfCar(int speedInkm){
+    cout << "Max speed of the car is: " << speedInkm << "km per hour" << endl;
+
+}
+
+int main(){
+    car mycar1; 
+    mycar1.speedOfCar(85); 
+  
+
+    return 0;
+}
