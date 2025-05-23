ba26cb8f83dbca5750395b7e62edf622fe8e6390
Siddharth Raj <rajsiddharth901@gmail.com>
2025-05-23 08:37:07 +0530

    classes.cpp file updated

--- self_progress/classes.cpp
+++ self_progress/classes.cpp
@@ -10,15 +10,24 @@ class Car{
     int year;
 
 };
+    class Computer{
+    public:
+    string company;
+    int price;
+};
 
 
 int main(){
     Car c1;
+    Computer lapy;
+    lapy.company="Lenovo";
+    lapy.price= 55000;
     c1.brand = "Audi";
     c1.model = "R8 E-tron";
     c1.year = 2005;
     
-    cout << "The car brand is " << c1.brand << " with model name as " << c1.model << ". It came to market on " << c1.year;
+    cout << "The car brand is " << c1.brand << " with model name as " << c1.model << ". It came to market on " << c1.year <<\n"";
+    cout<<lapy.company;
 
     return 0;
 }
