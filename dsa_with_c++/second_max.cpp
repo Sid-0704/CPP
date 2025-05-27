900650bd02366107b12bd1896d403597633e9c69
Siddharth Raj <rajsiddharth901@gmail.com>
2025-05-27 19:07:09 +0530

    second_max.cpp file added

---
+++ dsa_with_c++/second_max.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={6,9,8,3,2};
+
+    int ans = INT16_MIN;
+    int second = INT16_MIN;
+    int arr_size = sizeof(arr)/sizeof(int);
+    
+    for(int i=0;i<arr_size; i++){
+        if(arr[i]>ans){
+            ans = arr[i];
+        }
+    }
+
+    for(int m=0; m<arr_size; m++){
+        if(ans!=arr[m]){
+            second = max(second,arr[m]);
+        }
+    }
+    cout <<"The second max number in the array is: "<< second << endl;
+
+
+    return 0;
+}
