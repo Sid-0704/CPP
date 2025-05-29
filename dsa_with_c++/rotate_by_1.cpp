14fb0c83a6ed82b2e7b15dce295212a3919f0369
Siddharth Raj <rajsiddharth901@gmail.com>
2025-05-29 19:51:59 +0530

    rotate_by_1.cpp file added

---
+++ dsa_with_c++/rotate_by_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arrsize;
+
+    int arr[1000];
+    cout << "Enter the array size: ";
+    cin >> arrsize;
+
+    cout << "Enter the array elements: ";
+    for(int z=0; z<arrsize; z++){
+        cin >> arr[z];
+    }
+
+    int temp = arr[arrsize-1];
+    
+    for(int i=arrsize-2; i>=0; i--){
+
+        arr[i+1] = arr[i];
+    
+    }
+    arr[0] = temp;
+
+    cout << "\nArray after being rotated by 1 is:\n";
+    for(int m=0; m<arrsize; m++){
+        cout << arr[m] << endl;
+    }
+
+
+    return 0; 
+}
