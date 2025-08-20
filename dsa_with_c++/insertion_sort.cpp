39f2d80c5735b8a2e0abffb15af780a681c93a4e
Siddharth Raj <rajsiddharth901@gmail.com>
2025-08-20 15:10:54 +0530

    insertion_sort.cpp file added (modified for user given inputs)

--- dsa_with_c++/insertion_sort.cpp
+++ dsa_with_c++/insertion_sort.cpp
@@ -5,36 +5,30 @@ using namespace std;
 int main(){
     int arr[1000];
     int n;
-
-    // taking user input
-    cout << "Enter the size of array: ";
+    cout << "Enter the size of the array: ";
     cin >> n;
 
     cout << "Enter the elements: ";
     for(int m=0; m<n; m++){
         cin >> arr[m];
     }
-    
-    // sorting process
-    int last_element = n-1;
 
-    for(int i=0; i< n-1; i++){
+    for(int j=0; j<n-1; j++){
 
-        for(int j=0; j<last_element; j++){
-            if(arr[j] > arr[j+1]){
-                swap(arr[j], arr[j+1]);
+        for(int i=n-1; i>0; i--){
+
+            if(arr[i] < arr[i-1]){
+                swap(arr[i-1], arr[i]);
             }
+
         }
-        last_element--;
     }
-
-
 
     // printing the array
     cout << "The sorted array is: ";
     for(int k=0; k<n; k++){
         cout << arr[k] << " ";
     }
-
+    
     return 0;
 }
