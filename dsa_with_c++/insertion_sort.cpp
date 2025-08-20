2921bbf5f707bff995b48f40b45847f31f705465
Siddharth Raj <rajsiddharth901@gmail.com>
2025-08-20 19:40:37 +0530

    insertion_sort.cpp file updated
    user won't be able to access array out of bounds

--- dsa_with_c++/insertion_sort.cpp
+++ dsa_with_c++/insertion_sort.cpp
@@ -8,26 +8,36 @@ int main(){
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    cout << "Enter the elements: ";
-    for(int m=0; m<n; m++){
-        cin >> arr[m];
+    if(n > 1000){
+        cout << "Array size cannot exceed 1000" << endl;
+    }
+    else if(n < 0){
+        cout << "Array size cannot be negative" << endl;
     }
 
-    for(int j=0; j<n-1; j++){
-
-        for(int i=n-1; i>0; i--){
-
-            if(arr[i] < arr[i-1]){
-                swap(arr[i-1], arr[i]);
-            }
-
+    else{
+        cout << "Enter the elements: ";
+        for(int m=0; m<n; m++){
+            cin >> arr[m];
         }
-    }
 
-    // printing the array
-    cout << "The sorted array is: ";
-    for(int k=0; k<n; k++){
-        cout << arr[k] << " ";
+        for(int j=0; j<n-1; j++){
+
+            for(int i=n-1; i>0; i--){
+
+                if(arr[i] < arr[i-1]){
+                    swap(arr[i-1], arr[i]);
+                }
+
+            }
+        }
+
+        // printing the array
+        cout << "The sorted array is: ";
+        for(int k=0; k<n; k++){
+            cout << arr[k] << " ";
+        }
+
     }
     
     return 0;
